a2586ab6ddeb0b6fdd2465af0dca5bf9cd2a3c79
Safar1973 <fsafarm1@gmail.com>
2024-10-24 10:06:41 +0200

    Commit-Nachricht

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Faiek</title>
+    <title></title>
 </head>
 <body>
-    
+    <?php
+echo " ich liebe die Natur!";
+?>
+
 </body>
 </html>
